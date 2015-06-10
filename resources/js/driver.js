8da5e2866ebb613b4cf1e370cd1d58c62bf3f87f
Lawrence Moraira <lawrence@qburst.com>
2015-06-10 18:01:03 +0530

    Changed Google Charts to High Charts

--- resources/js/driver.js
+++ resources/js/driver.js
@@ -1,179 +1,208 @@
-$("input[name='driver']").on("change", function () {
-   	var driver = $("input[type='radio'][name='driver']:checked").val();
-	var option = $("input[type='radio'][name='options']:checked").val();
-	console.log(driver);
-	console.log(option);
-	drawChart(driver,option);
+$(function () {
+
+    var driverList = $.grep(drivers, function(element, index) {
+        return element;
+    });
+
+    var Days = $.grep(days, function(element, index) {
+        return element;
+    });
+
+    var ScoresData = new Array();
+    for(var i=0;i<driverList.length;i++) {
+        var jsonArg = new Object();
+        jsonArg.name = driverList[i].name;
+        jsonArg.data = driverList[i].score;
+        ScoresData.push(jsonArg);
+    }
+    
+    $('#charts').highcharts({
+        title: {
+            text: 'Driver Dashboard - Scores',
+            x: -20 //center
+        },
+        // subtitle: {
+        //     text: 'Source: WorldClimate.com',
+        //     x: -20
+        // },  
+        xAxis: {
+            categories: Days
+        },
+        yAxis: {
+            title: {
+                text: 'Scores'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        // tooltip: {
+        //     valueSuffix: '°C'
+        // },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: ScoresData
+    });
+    $('#container2').highcharts({
+        title: {
+            text: 'Driver Dashboard - Acceleration',
+            x: -20 //center
+        },
+        // subtitle: {
+        //     text: 'Source: WorldClimate.com',
+        //     x: -20
+        // },  
+        xAxis: {
+            categories: Days
+        },
+        yAxis: {
+            title: {
+                text: 'Acceleration'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        // tooltip: {
+        //     valueSuffix: '°C'
+        // },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: driverList[0].name,
+            data: driverList[0].acceleration
+        }, {
+            name: driverList[1].name,
+            data: driverList[1].acceleration
+        }]
+    });
+    $('#container3').highcharts({
+        title: {
+            text: 'Driver Dashboard - Breaks',
+            x: -20 //center
+        },
+        // subtitle: {
+        //     text: 'Source: WorldClimate.com',
+        //     x: -20
+        // },  
+        xAxis: {
+            categories: Days
+        },
+        yAxis: {
+            title: {
+                text: 'Breaks'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        // tooltip: {
+        //     valueSuffix: '°C'
+        // },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: driverList[0].name,
+            data: driverList[0].break
+        }, {
+            name: driverList[1].name,
+            data: driverList[1].break
+        }]
+    });
+    $('#container4').highcharts({
+        title: {
+            text: 'Driver Dashboard - Sharp Turns',
+            x: -20 //center
+        },
+        // subtitle: {
+        //     text: 'Source: WorldClimate.com',
+        //     x: -20
+        // },  
+        xAxis: {
+            categories: Days
+        },
+        yAxis: {
+            title: {
+                text: 'Sharp Turns'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        // tooltip: {
+        //     valueSuffix: '°C'
+        // },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: driverList[0].name,
+            data: driverList[0].turn
+        }, {
+            name: driverList[1].name,
+            data: driverList[1].turn
+        }]
+    });
+    $('#container5').highcharts({
+        title: {
+            text: 'Driver Dashboard - Crashes',
+            x: -20 //center
+        },
+        // subtitle: {
+        //     text: 'Source: WorldClimate.com',
+        //     x: -20
+        // },  
+        xAxis: {
+            categories: Days
+        },
+        yAxis: {
+            title: {
+                text: 'Crashes'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        // tooltip: {
+        //     valueSuffix: '°C'
+        // },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: driverList[0].name,
+            data: driverList[0].crash
+        }, {
+            name: driverList[1].name,
+            data: driverList[1].crash
+        }]
+    });
 });
-
-$("input[name='options']").on("change", function () {
-   	var driver = $("input[type='radio'][name='driver']:checked").val();
-	var option = $("input[type='radio'][name='options']:checked").val();
-	console.log(driver);
-	console.log(option);
-	drawChart(driver,option);
-});
-
-google.load('visualization', '1.1', {packages: ['corechart']});
-google.setOnLoadCallback(ScoresChart);
-/*
-function drawChart(driver,option) {
-	
-	if(driver == 'driverall') {
-		switch(option) {
-			case 'score': 		google.load('visualization', '1.1', {packages: ['corechart']});
-								google.setOnLoadCallback(ScoresChart);
-								break;
-			case 'acceleration':google.load('visualization', '1.1', {packages: ['corechart']});
-								google.setOnLoadCallback(AccelerationChart);
-								break;
-			case 'breakdowns': 	google.setOnLoadCallback(BreakChart);
-								break;
-			case 'turns': 		google.setOnLoadCallback(TurnChart);
-								break;
-			case 'crashes':		google.setOnLoadCallback(CrashChart);
-								break;			
-		}
-	}
-}
-*/
-function ScoresChart() {
-   var data = new google.visualization.DataTable();
-   data.addColumn('string', 'Day');
-   var options = {
-      title: 'Driver Scores',
-      vAxes: {
-         0: {title: 'Scores'}
-      }
-   };
-
-   $.ajax({
-      url: "../assets/json/data.json",
-      dataType: "JSON"
-   }).done(function(array) {
-      $.each(array.drivers, function() {
-         data.addColumn('number', this.name);
-      });
-      for(var j=0;j<array.drivers[0].score.length;j++) {
-         var a = array.days[j];
-         var b = array.drivers[0].score[j];
-         var c = array.drivers[1].score[j];
-         data.addRows([[a, b, c]]);
-      }
-      var chart = new google.visualization.LineChart(document.getElementById('charts'));
-      chart.draw(data, options);
-   });
-}
-/*
-function AccelerationChart() {
-   var data = new google.visualization.DataTable();
-   data.addColumn('string', 'Day');
-   var options = {
-      title: 'Event - Acceleration',
-      vAxes: {
-         0: {title: 'Acceleration'}
-      }
-   };
-
-   $.ajax({
-      url: "../assets/json/data.json",
-      dataType: "JSON"
-   }).done(function(array) {
-      $.each(array.drivers, function() {
-         data.addColumn('number', this.name);
-      });
-      for(var j=0;j<array.drivers[0].score.length;j++) {
-         var a = array.days[j];
-         var b = array.drivers[0].acceleration[j];
-         var c = array.drivers[1].acceleration[j];
-         data.addRows([[a, b, c]]);
-      }
-      var chart = new google.visualization.LineChart(document.getElementById('charts'));
-      chart.draw(data, options);
-   });   
-}
-
-function BreakChart() {
-   var data = new google.visualization.DataTable();
-   data.addColumn('string', 'Day');
-   var options = {
-      title: 'Event - Break',
-      vAxes: {
-         0: {title: 'Break'}
-      }
-   };
-
-   $.ajax({
-      url: "../assets/json/data.json",
-      dataType: "JSON"
-   }).done(function(array) {
-      $.each(array.drivers, function() {
-         data.addColumn('number', this.name);
-      });
-      for(var j=0;j<array.drivers[0].score.length;j++) {
-         var a = array.days[j];
-         var b = array.drivers[0].break[j];
-         var c = array.drivers[1].break[j];
-         data.addRows([[a, b, c]]);
-      }
-      var chart = new google.visualization.LineChart(document.getElementById('charts'));
-      chart.draw(data, options);
-   });   
-}
-
-function TurnChart() {
-   var data = new google.visualization.DataTable();
-   data.addColumn('string', 'Day');
-   var options = {
-      title: 'Event - Sharp Turn',
-      vAxes: {
-         0: {title: 'Sharp Turns'}
-      }
-   };
-
-   $.ajax({
-      url: "../assets/json/data.json",
-      dataType: "JSON"
-   }).done(function(array) {
-      $.each(array.drivers, function() {
-         data.addColumn('number', this.name);
-      });
-      for(var j=0;j<array.drivers[0].score.length;j++) {
-         var a = array.days[j];
-         var b = array.drivers[0].turn[j];
-         var c = array.drivers[1].turn[j];
-         data.addRows([[a, b, c]]);
-      }
-      var chart = new google.visualization.LineChart(document.getElementById('charts'));
-      chart.draw(data, options);
-   });   
-}
-
-function CrashChart() {
-   var data = new google.visualization.DataTable();
-   data.addColumn('string', 'Day');
-   var options = {
-      title: 'Event - Crash',
-      vAxes: {
-         0: {title: 'Crash'}
-      }
-   };
-
-   $.ajax({
-      url: "../assets/json/data.json",
-      dataType: "JSON"
-   }).done(function(array) {
-      $.each(array.drivers, function() {
-         data.addColumn('number', this.name);
-      });
-      for(var j=0;j<array.drivers[0].score.length;j++) {
-         var a = array.days[j];
-         var b = array.drivers[0].crash[j];
-         var c = array.drivers[1].crash[j];
-         data.addRows([[a, b, c]]);
-      }
-      var chart = new google.visualization.LineChart(document.getElementById('charts'));
-      chart.draw(data, options);
-   });   
-}
-*/
