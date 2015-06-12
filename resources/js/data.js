44f9fe8887e3be61043f237b63d36133815e2395
Lawrence Moraira <lawrence@qburst.com>
2015-06-12 12:05:14 +0530

    Drivers Option

--- resources/js/data.js
+++ resources/js/data.js
@@ -1,20 +1,24 @@
 var	drivers = [
 	{
-		"name": "Driver 1",
-		"score": [75,87,41,76,89,69,81],
-		"acceleration": [7,6,2,8,1,4,6],
-		"break": [9,7,4,5,1,6,8],
-		"turn": [3,5,4,7,8,1,5],
-		"crash": [3,2,5,1,5,3,6]
-	},
-	{
-		"name": "Driver 2",
-		"score": [87,41,76,89,69,81,75],
-		"acceleration": [6,2,8,1,4,6,7],
-		"break": [7,4,5,1,6,8,9],
-		"turn": [5,4,7,8,1,5,3],
-		"crash": [2,5,1,5,3,6,3]
-	}
+			"name": "M Schumacher",
+			"id": "driver1",
+			"score": [75,87,41,76,89,69,81],
+			"acceleration": [7,6,2,8,1,4,6],
+			"break": [9,7,4,5,1,6,8],
+			"turn": [3,5,4,7,8,1,5],
+			"crash": [3,2,5,1,5,3,6],
+			"image":"../assets/images/schumacher.jpg"
+		},
+		{
+			"name": "Valentino Rossi",
+			"id": "driver2",
+			"score": [87,41,76,89,69,81,75],
+			"acceleration": [6,2,8,1,4,6,7],
+			"break": [7,4,5,1,6,8,9],
+			"turn": [5,4,7,8,1,5,3],
+			"crash": [2,5,1,5,3,6,3],
+			"image":"../assets/images/rossi.jpg"
+		}
 
 ];
 
